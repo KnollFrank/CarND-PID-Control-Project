7a588c646dae7b696e1de76c0e277a7b3a9528d7
Frank Knoll <Knoll_Frank@web.de>
2018-10-22 22:13:01 +0200

    refactoring

--- src/tests/PIDtest.h
+++ src/tests/PIDtest.h
@@ -1,10 +1,17 @@
 #ifndef TESTS_PIDTEST_H_
 #define TESTS_PIDTEST_H_
 
+#include "../PID.h"
+
 #include <gtest/gtest.h>
+
+double getNewProcessVariable(const double PV, double output) {
+  return PV + output * 0.20 - PV * 0.10;
+}
 
 TEST(PIDTest, shouldGetOutput) {
   // GIVEN
+  PID pid;
 
   // WHEN
 
